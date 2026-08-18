9947648f01fcef2429d3eac7dda749191c02fdd2
ShauThongHo <161423722+ShauThongHo@users.noreply.github.com>
2026-08-18 14:09:41 +0800

    Update Project.java

--- Project.java
+++ Project.java
@@ -2,7 +2,6 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import BorrowBook.bookManager;
-import ManageData.*;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
